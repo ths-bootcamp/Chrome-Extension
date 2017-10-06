5ceaf5cbd978aaaeb185e60834c32722ad5a0a50
ak-zul <ak.zul65@gmail.com>
2017-10-06 17:22:25 +0530

    yeah! background and content files are working

--- extension/background.js
+++ extension/background.js
@@ -2,6 +2,8 @@
 // It matches URLs like: http[s]://[...]stackoverflow.com[...]
 var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
 
+
+console.log(1234141)
 // A function to use as callback
 function doStuffWithDom(domContent) {
     console.log('I received the following DOM content:\n' + domContent);
@@ -12,8 +14,8 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     // ...check the URL of the active tab against our pattern and...
     console.log(tab);
     console.log('wet')
-    if (urlRegex.test(tab.url)) {
+    // if (urlRegex.test(tab.url)) {
         // ...if it matches, send a message specifying a callback too
         chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
-    }
+    // }
 });
