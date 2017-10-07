5cf39dc8eff3486aa640a5a454ad1be94c22fef1
ak-zul <ak.zul65@gmail.com>
2017-10-07 13:47:08 +0530

    Added the logic for filter and an array list of badwords :)

--- extension/background.js
+++ extension/background.js
@@ -1,21 +1,20 @@
 // Regex-pattern to check URLs against.
 // It matches URLs like: http[s]://[...]stackoverflow.com[...]
-var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
 
 
-console.log(1234141)
+//console.log(1234141)
 // A function to use as callback
 function doStuffWithDom(domContent) {
     console.log('I received the following DOM content:\n' + domContent);
 }
 
 // When the browser-action button is clicked...
-chrome.browserAction.onClicked.addListener(function (tab) {
-    // ...check the URL of the active tab against our pattern and...
-    console.log(tab);
-    console.log('wet')
-    // if (urlRegex.test(tab.url)) {
-        // ...if it matches, send a message specifying a callback too
-        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
-    // }
-});
+// chrome.webNavigation.onCompleted.addListener(function (tab) {
+//     // ...check the URL of the active tab against our pattern and...
+//     //console.log(tab);
+//     //console.log('wet')
+//     // if (urlRegex.test(tab.url)) {
+//         // ...if it matches, send a message specifying a callback too
+//         chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+//     // }
+// });
